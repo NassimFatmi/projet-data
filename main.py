5da68975f8ba7753f62ec95062e72ecfe658a287
nassim.fatmi <nassim.fatmi@universite-paris-saclay.fr>
2023-12-15 16:02:02 +0100

    premiere partie

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import pandas as pd
 import yfinance as yf
-from datetime import datetime as dt
-import pytz
 
 
-data = yf.download("AAPL.PA")
+data = yf.download("TSLA", start="2022-01-01", end="2022-12-31")
 
 print(data)
